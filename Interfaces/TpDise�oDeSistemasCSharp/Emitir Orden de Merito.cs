5bbceb03551bc848eb962843afc1ed0e964b0873
lmhsjackson <lmhsjackson@gmail.com>
2012-01-17 18:19:07 -0200

    El boton Buscar y el boton emitir orden de merito hacen lo que deben.

--- Interfaces/TpDise�oDeSistemasCSharp/Emitir Orden de Merito.cs
+++ Interfaces/TpDise�oDeSistemasCSharp/Emitir Orden de Merito.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Gestores;
+using Entidades;
 
 namespace TpDiseñoCSharp
 {
@@ -24,7 +26,30 @@ namespace TpDiseñoCSharp
 
         private void Buscar_Click(object sender, EventArgs e)
         {
+            AdministradorBD AdminBD = new AdministradorBD();
+            
+            List<Puesto> Lista_de_puestos;
+
+            Lista_de_puestos = AdminBD.recuperarPuestos(this.codigo.Text.ToString(), this.nombre.Text.ToString(), this.empresa.Text.ToString());
+
+            ResultadosDeBusqueda.DataSource = Lista_de_puestos;
+
             ResultadosDeBusqueda.Visible = true;
         }
+
+        private void EmitirOrdenDeMerito_Click(object sender, EventArgs e)
+        {
+            int numero_fila_seleccionada = ResultadosDeBusqueda.CurrentRow.Index;
+            
+            string codigo_puesto_seleccionado = ResultadosDeBusqueda[0, numero_fila_seleccionada].Value.ToString();
+
+            List<Puesto> Lista_puestos = (List<Puesto>) ResultadosDeBusqueda.DataSource;
+            
+            Puesto puesto_seleccionado = Lista_puestos.Find(delegate(Puesto p) { return p.Codigo == codigo_puesto_seleccionado; });
+            
+
+
+        }
+
     }
 }
