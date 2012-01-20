a58691f1f971e7afd047bc249255c507fe823237
lmhsjackson <lmhsjackson@gmail.com>
2012-01-20 17:10:34 -0200

    Actualizado bastante la ventana de seleccion de evaluaciones (CU emitir orden de merito) CON CHECKBOX

---
+++ Interfaces/TpDise�oDeSistemasCSharp/Seleccion de evaluaciones.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace TpDiseñoCSharp
+{
+    partial class Seleccion_de_evaluaciones
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel_evaluaciones = new System.Windows.Forms.Panel();
+            this.todos = new System.Windows.Forms.Button();
+            this.ninguno = new System.Windows.Forms.Button();
+            this.aceptar = new System.Windows.Forms.Button();
+            this.puesto = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel_evaluaciones
+            // 
+            this.panel_evaluaciones.AutoSize = true;
+            this.panel_evaluaciones.Location = new System.Drawing.Point(12, 38);
+            this.panel_evaluaciones.Name = "panel_evaluaciones";
+            this.panel_evaluaciones.Size = new System.Drawing.Size(975, 509);
+            this.panel_evaluaciones.TabIndex = 0;
+            // 
+            // todos
+            // 
+            this.todos.Location = new System.Drawing.Point(993, 42);
+            this.todos.Name = "todos";
+            this.todos.Size = new System.Drawing.Size(75, 26);
+            this.todos.TabIndex = 1;
+            this.todos.Text = "Todos";
+            this.todos.UseVisualStyleBackColor = true;
+            this.todos.Click += new System.EventHandler(this.todos_Click);
+            // 
+            // ninguno
+            // 
+            this.ninguno.Location = new System.Drawing.Point(993, 88);
+            this.ninguno.Name = "ninguno";
+            this.ninguno.Size = new System.Drawing.Size(75, 27);
+            this.ninguno.TabIndex = 2;
+            this.ninguno.Text = "Ninguno";
+            this.ninguno.UseVisualStyleBackColor = true;
+            this.ninguno.Click += new System.EventHandler(this.ninguno_Click);
+            // 
+            // aceptar
+            // 
+            this.aceptar.Location = new System.Drawing.Point(993, 453);
+            this.aceptar.Name = "aceptar";
+            this.aceptar.Size = new System.Drawing.Size(113, 62);
+            this.aceptar.TabIndex = 3;
+            this.aceptar.Text = "Aceptar";
+            this.aceptar.UseVisualStyleBackColor = true;
+            // 
+            // puesto
+            // 
+            this.puesto.AutoSize = true;
+            this.puesto.Location = new System.Drawing.Point(39, 9);
+            this.puesto.Name = "puesto";
+            this.puesto.Size = new System.Drawing.Size(65, 17);
+            this.puesto.TabIndex = 4;
+            this.puesto.Text = "PUESTO";
+            // 
+            // Seleccion_de_evaluaciones
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1291, 646);
+            this.Controls.Add(this.puesto);
+            this.Controls.Add(this.aceptar);
+            this.Controls.Add(this.ninguno);
+            this.Controls.Add(this.todos);
+            this.Controls.Add(this.panel_evaluaciones);
+            this.Name = "Seleccion_de_evaluaciones";
+            this.Text = "Elija las evaluaciones para las que desea emitir un Reporte de Meritos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel_evaluaciones;
+        private System.Windows.Forms.Button todos;
+        private System.Windows.Forms.Button ninguno;
+        private System.Windows.Forms.Button aceptar;
+        private System.Windows.Forms.Label puesto;
+
+    }
+}
---
+++ Interfaces/TpDise�oDeSistemasCSharp/Seleccion de evaluaciones.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Entidades;
+using Gestores;
+
+namespace TpDiseñoCSharp
+{
+    public partial class Seleccion_de_evaluaciones : Form
+    {
+        public Seleccion_de_evaluaciones(List<PuestoEvaluado> Lista_puestos_ev)
+        {
+            InitializeComponent();
+            panel_evaluaciones.AutoSize = true;
+            
+            //for (int j = 0; j < 50; j++) //descomenta esta linea si queres ver como funciona cuando hay MUCHAS evaluaciones. (autoscroll)
+
+            for (int i = 0; i < Lista_puestos_ev.Count; i++)
+            {
+                CheckBox check = new CheckBox();
+                check.Width = 20;
+                Label renglon = new Label();
+                
+                check.Location = new Point(0, i * 30); //si usas el for de 50 aca cabia la i por una j
+                renglon.Location = new Point(30, i * 30); //si usas el for de 50 aca cabia la i por una j
+
+                renglon.Width = 2000;
+                renglon.Text = Lista_puestos_ev[i].Nombre.ToString() + " " + Lista_puestos_ev[i].Codigo.ToString() + " " + Lista_puestos_ev[i].Empresa.ToString() + " " + Lista_puestos_ev[i].Fecha_Comienzo.ToShortDateString() + " " + Lista_puestos_ev[i].Fecha_Comienzo.ToShortTimeString();
+                
+                panel_evaluaciones.Controls.Add(check);
+                panel_evaluaciones.Controls.Add(renglon);
+            }
+            
+
+        }
+
+        private void todos_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < panel_evaluaciones.Controls.Count; i+=2)
+            {
+                CheckBox checkBox = (CheckBox) panel_evaluaciones.Controls[i];
+                checkBox.Checked = true;
+
+            }
+                
+        }
+
+        private void ninguno_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < panel_evaluaciones.Controls.Count; i += 2)
+            {
+                CheckBox checkBox = (CheckBox)panel_evaluaciones.Controls[i];
+                checkBox.Checked = false;
+
+            }
+        }
+
+       
+        
+    }
+}
